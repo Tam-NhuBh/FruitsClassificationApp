86ee326f04d2555cc4ef88b49a3c62a49ca0bb3c
Tam-NhuBh <tnhubh@gmail.com>
2023-05-09 13:46:15 +0700

    New update UI

--- app/src/main/java/hcmute/edu/vn/fruitsclassificationapp/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/fruitsclassificationapp/MainActivity.java
@@ -9,6 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.layout_register);
         setContentView(R.layout.activity_main);
+//        setContentView(R.layout.layout_listdishes);
+//        setContentView(R.layout.activity_main);
+
+
     }
 }
