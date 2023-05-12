2716961a72bdaafc7e469d0751e220bca93b62ba
TrungNguyen <nguyennguyen6616634@gmail.com>
2023-05-12 08:55:56 +0700

    nguyen_login_register_forgot

--- app/src/main/java/hcmute/edu/vn/fruitsclassificationapp/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/fruitsclassificationapp/MainActivity.java
@@ -2,10 +2,15 @@ package hcmute.edu.vn.fruitsclassificationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgSignout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.layout_listdishes);
 //        setContentView(R.layout.activity_main);
 
+        imgSignout = findViewById(R.id.imgLogout);
+        imgSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                FirebaseAuth.getInstance().signOut();
+                finish();
+            }
+        });
 
     }
 }
